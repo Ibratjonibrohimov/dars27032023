5b31d30dee79f20be7f39193159cf51f022d7fa4
Ibratjonibrohimov <ibratjonibroximov@gmail.com>
2023-03-27 16:11:40 +0500

    birinchi commit

---
+++ src/main/java/uz/najottalim/Main.java
@@ -0,0 +1,9 @@
+package uz.najottalim;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Git bilan ishlash!");
+        System.out.println("Bu birinchi  git  loyiham !");
+
+    }
+}
