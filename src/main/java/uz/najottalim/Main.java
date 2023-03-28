709b08323589786f56fe963e0abb2b290745b027
Ibratjonibrohimov <ibratjonibroximov@gmail.com>
2023-03-28 14:52:02 +0500

    commit2

--- src/main/java/uz/najottalim/Main.java
+++ src/main/java/uz/najottalim/Main.java
@@ -1,10 +1,12 @@
 package uz.najottalim;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Git bilan ishlash!");
         System.out.println("Bu birinchi  git  loyiham !");
         System.out.println("1- o'zgarish");
-        System.out.println("Umarbek top");
+        System.out.println("2-ozgarish");
     }
 }
